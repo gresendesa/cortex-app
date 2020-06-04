620eb5f3a308b847bf2a20e1f7d4c096ad499f5a
Guilherme Sá <gresendesa@gmail.com>
2020-06-04 09:46:38 -0300

    trigger ok

--- src/App.js
+++ src/App.js
@@ -4,36 +4,42 @@ import ButtonAppBar from './components/NavBar';
 import { Container } from '@material-ui/core';
 import { foo } from './mock/processes'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import TriggerEditor from './Trigger'
 
 function App() {
 
 	var tasks = foo.macro.tasks;
 
 	return (
+
+
+		<React.Fragment>
+
+			<ButtonAppBar />
+			<Container maxWidth="sm">
+
+				<Router>
+					
+					<Route exact path="/">
+
+					  	<h2>Process <small>{ foo.name }</small> </h2>
+					    <TaskList tasks={tasks} />
+						
+					</Route>
+
+					<Route exact path="/trigger">
+
+					  	<h2>Trigger </h2>
+					    <TriggerEditor />
+						
+					</Route>
+
+				</Router>
+
+			</Container>
+
+		</React.Fragment>
 		
-		<Router>
-			
-			<Route exact path="/">
-
-				<React.Fragment>
-
-				  	<ButtonAppBar />
-
-				  	<Container maxWidth="sm">
-
-				  		<h2>Process <small>{ foo.name }</small> </h2>
-
-				      	<TaskList tasks={tasks} />
-
-				  	</Container>
-
-				</React.Fragment>
-			</Route>
-
-		</Router>
-			
-		
-
 	);
 }
 
---
+++ src/Trigger.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class TriggerEditor extends React.Component {
+
+
+	constructor(){
+		super();
+	}
+
+	render(){
+
+		return (
+
+			<div>
+				<h3>opa</h3>
+			</div>
+
+		)
+
+	}
+
+}
+
+export default TriggerEditor;
